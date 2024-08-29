516016affa378508e2f48b0c57d5f93ef0d84918
AyiStar <ayistar@outlook.com>
2024-08-30 00:15:45 +0800

    naive quantized linear

--- examples/quant_tinyllama.py
+++ examples/quant_tinyllama.py
@@ -1,2 +1,14 @@
+import os
+
+import torch
 from transformers import LlamaForCausalLM, LlamaTokenizer
 
+model_path = os.environ.get("LLAMA_MODEL_PATH")
+if model_path is None:
+    raise EnvironmentError(
+        "The environment variable 'LLAMA_MODEL_PATH' is not set or is invalid."
+    )
+model = LlamaForCausalLM.from_pretrained(model_path, torchscript=True, torch_dtype=torch.float32)
+model.config.use_cache = False
+
+print(model)
--- torch_quant/linear.py
+++ torch_quant/linear.py
@@ -6,23 +6,23 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-class QuantLinear(nn.Module):
+class QuantizedLinear(nn.Module):
     
-    def __init__(self, in_features: int, out_features: int, quantized_weight: torch.Tensor, quantized_bias: Optional[torch.Tensor]=None) -> None:
+    def __init__(
+        self,
+        in_features: int,
+        out_features: int,
+        weight_quant: torch.Tensor,
+        weight_scale: torch.Tensor) -> None:
+        super().__init__()
         self.in_features = in_features
         self.out_features = out_features
-        self.weight = nn.Parameter(quantized_weight)
-        if quantized_bias is not None:
-            self.bias = nn.Parameter(quantized_bias)
-        else:
-            self.register_parameter('bias', None)
+        self.weight_quant = nn.Parameter(weight_quant)
+        self.weight_scale = nn.Parameter(weight_scale)
     
     def forward(self, inputs):
-        float_weight = self._dequantize(self.weight)
-        float_bias = None
-        if self.bias:
-            float_bias = self._dequantize(self.bias)
-        outputs = F.linear(inputs, float_weight, float_bias)
+        float_weight = self._dequantize_tensor(self.weight_quant, self.weight_scale)
+        outputs = F.linear(inputs, float_weight)
         return outputs
     
     @staticmethod
@@ -33,11 +33,10 @@ class QuantLinear(nn.Module):
     def _quantize_tensor(inputs: torch.Tensor, narrow_range: bool=True):
         """Shared function body between TensorQuantFunction and FakeTensorQuantFunction"""
         # Fine scale, per channel scale will be handled by broadcasting, which could be tricky. Pop a warning.
+        amax = QuantizedLinear._get_amax(inputs)
         if isinstance(amax, torch.Tensor) and inputs.dim() != amax.dim():
             logging.debug("amax %s has different shape than inputs %s. Make sure broadcast works as expected!", amax.size(),
                         inputs.size())
-        
-        amax = QuantLinear._get_amax(inputs)
 
         # Computation must be in FP32 to prevent potential over flow.
         input_dtype = inputs.dtype
@@ -62,30 +61,38 @@ class QuantLinear(nn.Module):
             zero_amax_mask = (amax <= epsilon)
             scale[zero_amax_mask] = 0  # Value quantized with amax=0 should all be 0
 
-        outputs = torch.clamp((inputs * scale).round_(), min_bound, max_bound)
+        quant = torch.clamp((inputs * scale).round_(), min_bound, max_bound)
 
         if min_amax <= epsilon:
             scale[zero_amax_mask] = 1.  # Return 1 makes more sense for values quantized to 0 with amax=0
 
         if input_dtype == torch.half:
-            outputs = outputs.half()
+            quant = quant.half()
 
-        return outputs, scale
+        return quant, scale
     
-    def forward(self, inputs):
-        pass
+    @staticmethod
+    def _dequantize_tensor(quant: torch.Tensor, scale: torch.Tensor):
+        return (quant / scale).float()
     
     @classmethod
     def from_linear(cls, linear: nn.Linear):
+        assert linear.bias is None
         float_weight = linear.weight
-        float_bias = linear.bias
-        quantized_weight = cls._quantize_tensor(float_weight.data)
-        quantized_bias = None
-        if float_bias is not None:
-            quantized_bias = cls._quantize_tensor(float_bias.data)
+        weight_quant, weight_scale = cls._quantize_tensor(float_weight.data)
         return cls(
             in_features=linear.in_features,
             out_features=linear.out_features,
-            quantized_weight=quantized_weight,
-            quantized_bias=quantized_bias,
+            weight_quant=weight_quant,
+            weight_scale=weight_scale
         )
+
+
+if __name__ == '__main__':
+    inputs = torch.randn(4, 256)
+    float_linear = nn.Linear(in_features=256, out_features=8, bias=False)
+    quantized_linear = QuantizedLinear.from_linear(float_linear)
+    float_output = float_linear(inputs)
+    quantized_output = quantized_linear(inputs)
+    print(float_output)
+    print(quantized_output)
